f221b45640d3823276fec93310dfa5aa23f2ba86
afra818 <60651664+afra818@users.noreply.github.com>
2026-03-30 23:14:23 +0100

    f

--- run.sh
+++ run.sh
@@ -11,4 +11,15 @@
 module load anaconda3/2023.09
 source activate tecoa
 cd /scratch/hpc/07/zhang303/tecoa
-srun python finetuning.py --batch_size 64 --root /scratch/hpc/07/zhang303/tecoa/data --dataset ImageNet --name feimogu --train_eps 1 --train_numsteps 2 --train_stepsize 1
+srun python finetuning.py \
+  --batch_size 64 \
+  --num_workers 8 \
+  --root /scratch/hpc/07/zhang303/tecoa/data \
+  --dataset cifar100 \
+  --name feimogu \
+  --learning_rate 1e-5 \
+  --epochs 10 \
+  --train_eps 1 \
+  --train_numsteps 2 \
+  --train_stepsize 1
+  #   --imagenet_root /scratch/hpc/07/zhang303/tecoa/data/imagenet1k \

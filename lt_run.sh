eb69c20ade87435be759b5c91082620454c05a99
afra818 <60651664+afra818@users.noreply.github.com>
2026-03-31 14:28:54 +0100

    f

--- lt_run.sh
+++ lt_run.sh
@@ -35,21 +35,9 @@ cd /scratch/hpc/07/zhang303/tecoa
 # # 20688434
 
 
-# 3) CIFAR100-LT-100 对抗微调
-srun python at_lt_test.py \
-  --dataset cifar100_lt \
-  --num_workers 8 \
-  --imbalance_factor 100 \
-  --lt_seed 0 \
-  --epochs 10 \
-  --batch_size 16 \
-  --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
-  --test_eps 2 --test_numsteps 5 --test_stepsize 1\
-  --name CLIP_lt_finetuning_version_1
-
-# # 4) same-budget balanced 对照组
+# # 3) CIFAR100-LT-100 对抗微调
 # srun python at_lt_test.py \
-#   --dataset cifar100_balanced_subset \
+#   --dataset cifar100_lt \
 #   --num_workers 8 \
 #   --imbalance_factor 100 \
 #   --lt_seed 0 \
@@ -57,4 +45,17 @@ srun python at_lt_test.py \
 #   --batch_size 16 \
 #   --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
 #   --test_eps 2 --test_numsteps 5 --test_stepsize 1\
-#  --name CLIP_same-budget_balanced_finetuning_version_1
+#   --name CLIP_lt_finetuning_version_1
+# #   20688444
+
+# 4) same-budget balanced 对照组
+srun python at_lt_test.py \
+  --dataset cifar100_balanced_subset \
+  --num_workers 8 \
+  --imbalance_factor 100 \
+  --lt_seed 0 \
+  --epochs 10 \
+  --batch_size 16 \
+  --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
+  --test_eps 2 --test_numsteps 5 --test_stepsize 1\
+ --name CLIP_same-budget_balanced_finetuning_version_1

31b3b8d828c9a7e649b543ff8fc1cf73ee65b0b7
afra818 <60651664+afra818@users.noreply.github.com>
2026-03-31 14:26:23 +0100

    f

--- lt_run.sh
+++ lt_run.sh
@@ -12,39 +12,46 @@ module load anaconda3/2023.09
 source activate tecoa
 cd /scratch/hpc/07/zhang303/tecoa
 
+
 # 1) 原始 CLIP baseline（不微调）
-
-python at_lt_test.py \
-  --no_finetune \
-  --dataset cifar100 \
-  --batch_size 16 \
-  --test_eps 2 --test_numsteps 5 --test_stepsize 1\
-  --name ori_CLIP_baseline_nofinetunign_version_1
-
-# # 2) 标准 CIFAR100 对抗微调
-# python robust_finetune_cifar100_lt_experiments.py \
+# srun python at_lt_test.py \
+#   --no_finetune \
+#   --num_workers 8 \
 #   --dataset cifar100 \
-#   --epochs 10 \
 #   --batch_size 16 \
-#   --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
-#   --test_eps 2 --test_numsteps 5 --test_stepsize 1
+#   --test_eps 2 --test_numsteps 5 --test_stepsize 1\
+#   --name ori_CLIP_baseline_nofinetuning_version_1.1
+
+2) 标准 CIFAR100 对抗微调
+srun python at_lt_test.py \
+  --dataset cifar100 \
+  --num_workers 8 \
+  --epochs 10 \
+  --batch_size 16 \
+  --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
+  --test_eps 2 --test_numsteps 5 --test_stepsize 1\
+  --name standard_CLIP_finetuning_version_1
 
 # # 3) CIFAR100-LT-100 对抗微调
-# python robust_finetune_cifar100_lt_experiments.py \
+# srun python at_lt_test.py \
 #   --dataset cifar100_lt \
+#   --num_workers 8 \
 #   --imbalance_factor 100 \
 #   --lt_seed 0 \
 #   --epochs 10 \
 #   --batch_size 16 \
 #   --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
-#   --test_eps 2 --test_numsteps 5 --test_stepsize 1
+#   --test_eps 2 --test_numsteps 5 --test_stepsize 1\
+#  --name CLIP_lt_finetuning_version_1
 
 # # 4) same-budget balanced 对照组
-# python robust_finetune_cifar100_lt_experiments.py \
+# srun python at_lt_test.py \
 #   --dataset cifar100_balanced_subset \
+#   --num_workers 8 \
 #   --imbalance_factor 100 \
 #   --lt_seed 0 \
 #   --epochs 10 \
 #   --batch_size 16 \
 #   --train_eps 2 --train_numsteps 5 --train_stepsize 1 \
-#   --test_eps 2 --test_numsteps 5 --test_stepsize 1
+#   --test_eps 2 --test_numsteps 5 --test_stepsize 1\
+#  --name CLIP_same-budget_balanced_finetuning_version_1
